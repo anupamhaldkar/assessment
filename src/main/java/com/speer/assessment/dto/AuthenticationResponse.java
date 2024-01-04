f3060473d9e0203b416b3fa69f0478aa259ac6b8
AnupamHaldkar <ahdev2020@outlook.com>
2024-01-04 16:33:34 +0530

    Added Security Config and UserRepo

---
+++ src/main/java/com/speer/assessment/dto/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.speer.assessment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+
+}
---
+++ src/main/java/com/speer/assessment/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.speer.assessment.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.speer.assessment.entity.User;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+    
+}
